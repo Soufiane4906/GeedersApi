17e5bf918792e6908ecf3155cfaea60f3b382713
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-08-01 04:47:38 +0000

    re 1-8

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,6 +1,8 @@
 import createError from "../utils/createError.js";
 import Message from "../models/message.model.js";
 import Conversation from "../models/conversation.model.js";
+import User from "../models/user.model.js";
+
 
 export const createMessage = async (req, res, next) => {
   const newMessage = new Message({
@@ -27,13 +29,41 @@ export const createMessage = async (req, res, next) => {
     next(err);
   }
 };
+
 export const getMessages = async (req, res, next) => {
   try {
-    const messages = await Message.find({ conversationId: req.params.id
-    //get user.img and username from messages.userId
-    
+    const messages = await Message.find({ conversationId: req.params.id });
+
+    // Get unique user IDs from messages
+    const userIds = [...new Set(messages.map(message => message.userId))];
+    const users = await User.find({ _id: { $in: userIds } });
+
+    // Map user data to each message
+    const messagesWithUserDetails = messages.map(message => {
+      const user = users.find(user => user._id.toString() === message.userId.toString());
+      return {
+        ...message.toObject(),
+        user: user || { img: "https://via.placeholder.com/50", username: "Unknown" } // Default values if user not found
+      };
     });
-    res.status(200).send(messages);
+
+    // Fetch conversation details to get buyer and seller details
+    const conversation = await Conversation.findOne({ id: req.params.id });
+    if (!conversation) {
+      return res.status(404).send('Conversation not found');
+    }
+
+    // Fetch buyer and seller details
+    const [buyer, seller] = await Promise.all([
+      User.findById(conversation.buyerId),
+      User.findById(conversation.sellerId)
+    ]);
+
+    res.status(200).json({
+      messages: messagesWithUserDetails,
+      buyer: buyer || { img: "https://via.placeholder.com/100", username: "Unknown", email: "", phone: "" },
+      seller: seller || { img: "https://via.placeholder.com/100", username: "Unknown", email: "", phone: "" }
+    });
   } catch (err) {
     next(err);
   }
