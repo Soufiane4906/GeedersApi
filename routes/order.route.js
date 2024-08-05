94f547e1d075c9d4e5b988373768e3210cd7ad16
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-08-05 03:23:15 +0000

    REMOVE TOKEN

--- routes/order.route.js
+++ routes/order.route.js
@@ -5,10 +5,10 @@ import { getOrders, intent, confirm , getOrder} from "../controllers/order.contr
 const router = express.Router();
 
 // router.post("/:gigId", verifyToken, createOrder);
-router.get("/", verifyToken, getOrders);
-router.post("/create-payment-intent/:id", verifyToken, intent);
-router.put("/", verifyToken, confirm);
+router.get("/", getOrders);
+router.post("/create-payment-intent/:id", intent);
+router.put("/", confirm);
 //get single order
- router.get("/:id", verifyToken, getOrder);
+ router.get("/:id", getOrder);
 
 export default router;
