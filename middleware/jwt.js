7c687dd3ad75c481a8348517ff65c264191a3ed4
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2024-08-05 03:18:58 +0000

    TEST 318

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -4,7 +4,7 @@ import createError from "../utils/createError.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) return next(createError(401,"You are not authenticated!"))
-
+    console.log("ttttttttttttttoooooooooooken",token);
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return next(createError(403,"Token is not valid!"))
